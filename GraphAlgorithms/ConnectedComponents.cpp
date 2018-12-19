bdf64507141ac2a53fcb6277b0788772e02a5ace
Vishal Patel <vishalp@uber.com>
2018-12-19 15:16:39 -0800

    Updated

--- GraphAlgorithms/ConnectedComponents.cpp
+++ GraphAlgorithms/ConnectedComponents.cpp
@@ -26,7 +26,7 @@ void connectedComponents(int u){
     for ( int j = 0 ; j < (int)AdjList[u].size(); j++){
         ii v = AdjList[u][j];
         if ( dfs_num[v.first] == UNVISITED ){
-            dfs(v.first);
+            connectedComponents(v.first);
         }
     }
 
